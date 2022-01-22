d5191e2794ac9161d682868f6f10bb69de7df784
yaswankar <yaswankar1998@gmail.com>
2022-01-22 17:31:52 +0530

    Changes: First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Todo List</h1>
+    <div class="inputSection">
+      <input type="text" v-model="itemName" />
+      <button class="add-btn" @click="addItem">ADD</button>
+    </div>
+    <List :items="items" @removeItem="removeFromList"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import List from './components/List.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      itemName: "",
+      items: []
+    };
+  },
   components: {
-    HelloWorld
+    List
+  },
+  methods: {
+    removeFromList(idx) {
+      this.items.splice(idx, 1);
+    },
+    addItem() {
+      if(this.itemName == "") return;
+      this.items.push({
+        name: this.itemName
+      });
+      this.itemName = "";
+    }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +46,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  .inputSection {
+    .add-btn {
+      margin-left: 10px;
+    }
+  }
 }
 </style>
---
+++ src/components/List.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="list">
+    <table>
+      <thead>
+        <th>Sno</th>
+        <th>Items</th>
+        <th>Actions</th>
+      </thead>
+      <tr v-for="(item, idx) in items" :key="idx">
+        <td>{{idx}}</td>
+        <td>{{item.name}}</td>
+        <td>
+          <span>
+            <button @click="removeItem(idx)">X</button>
+          </span>
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  props: {
+    items: {
+      type: Array,
+      default: () => {[]}
+    }
+  },
+  methods: {
+    removeItem(idx) {
+      this.$emit('removeItem', idx);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
