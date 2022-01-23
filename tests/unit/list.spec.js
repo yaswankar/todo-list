d9df55028c6e9783ee3ff771df1b2c5389d89bb7
yaswankar <yaswankar1998@gmail.com>
2022-01-23 18:14:31 +0530

    Changes: Updated test cases for list component

--- tests/unit/list.spec.js
+++ tests/unit/list.spec.js
@@ -1,9 +1,20 @@
-import { shallowMount } from "@vue/test-utils";
+import { mount } from "@vue/test-utils";
 import List from "@/components/List.vue";
 
 describe("List.vue", () => {
-    const wrapper = shallowMount(List);
+    const wrapper = mount(List, {
+        propsData: {
+            items: []
+        }
+    });
     it('Rendering List div', () => {
         expect(wrapper.contains('.list')).toBe(true);
+    })
+    it('Renders table and header row', () => {
+        expect(wrapper.contains('table')).toBe(true);
+        expect(wrapper.contains('table thead')).toBe(true);
+    })
+    it('Checking props', () => {
+        expect(Array.isArray(wrapper.props().items)).toBe(true);
     })
 })
